0b38c2580350da9a0c62673d569f4b77daf7ead3
m45adiwinata <m45adiwinata@gmail.com>
2018-01-03 23:25:04 +0800

    Commit kedua gan

--- main.cpp
+++ main.cpp
@@ -12,14 +12,14 @@
 #define LEFT  3
 #define RIGHT 4
 
-char title[] = "OpenGL Snake";
+char title[] = "Snake the Game";
 
 float map_half_length = 30.0f;
 
 int direction = DOWN;
-int move_speed = 100;
+int move_speed = 150;
 bool moved = false;
-std::deque< std::deque<float> > part_coords;
+std::deque< std::deque<float> > snake_body_coords;
 
 bool food_available = false;
 int food_coords[2];
@@ -37,9 +37,9 @@ void spawnFood() {
 				food_coords[1] = 2 * (rand() % ((int)map_half_length + 1)) - (int)map_half_length };
 
 			// Does it collide with the snake?
-			for (unsigned int a = 0; a < part_coords.size(); a++) {
-				if (temp_food_coords[0] == part_coords[a][0] &&
-					temp_food_coords[1] == part_coords[a][1]) {
+			for (unsigned int a = 0; a < snake_body_coords.size(); a++) {
+				if (temp_food_coords[0] == snake_body_coords[a][0] &&
+					temp_food_coords[1] == snake_body_coords[a][1]) {
 					collides = true;
 				}
 			}
@@ -58,7 +58,7 @@ void spawnFood() {
 
 	glLoadIdentity();
 	glTranslatef(food_coords[0], food_coords[1], -40.0f);
-	glColor3f(1.0f, 0.0f, 0.0f);
+	glColor3f(1.0f, 1.0f, 0.0f);
 
 	glBegin(GL_POLYGON);
 	glVertex2d(1.0f, 1.0f);
@@ -75,10 +75,10 @@ void display() {
 	// The vertex order is clockwise
 	// The side order is front, back, left, right, top, bottom (if applicable)
 
-	// Loop over snake size and draw each part at it's respective coordinates
-	for (unsigned int a = 0; a < part_coords.size(); a++) {
+	// Loop over snake size and draw each part
+	for (unsigned int a = 0; a < snake_body_coords.size(); a++) {
 		glLoadIdentity();
-		glTranslatef(part_coords[a][0], part_coords[a][1], -40.0f);
+		glTranslatef(snake_body_coords[a][0], snake_body_coords[a][1], -40.0f);
 		glColor3f(0.0f, 1.0f, 0.0f);
 
 		glBegin(GL_POLYGON);
@@ -97,113 +97,117 @@ void display() {
 void moveSnake(int new_direction) {
 	direction = new_direction;
 
-	int last_part = part_coords.size() - 1;
-	std::deque<float> new_head = part_coords[last_part];
+	int last_part = snake_body_coords.size() - 1;
+	std::deque<float> new_head = snake_body_coords[last_part];
 
 	if (direction == UP) {
-		// Did we slither into ourself?
-		for (unsigned int a = 0; a < part_coords.size(); a++) {
-			if (part_coords[0][0] == part_coords[a][0] &&
-				part_coords[0][1] + 2.0f == part_coords[a][1]) {
+		// Snake hits itself?
+		for (unsigned int a = 0; a < snake_body_coords.size(); a++) {
+			if (snake_body_coords[0][0] == snake_body_coords[a][0] &&
+				snake_body_coords[0][1] + 2.0f == snake_body_coords[a][1]) {
 				exit(0);
 			}
 		}
 
-		// Did we slither into a wall?
-		if (part_coords[0][1] == map_half_length) {
+		// Snake hits the wall?
+		if (snake_body_coords[0][1] == map_half_length) {
 			exit(0);
 		}
 
-		// Did we get food?
-		if (part_coords[0][0] == food_coords[0] &&
-			part_coords[0][1] + 2.0f == food_coords[1]) {
+		// Snake gets the food?
+		if (snake_body_coords[0][0] == food_coords[0] &&
+			snake_body_coords[0][1] + 2.0f == food_coords[1]) {
 			growth_stage++;
+			move_speed-=10;
 			food_available = false;
 		}
 
-		new_head[1] = part_coords[0][1] + 2.0f;
+		new_head[1] = snake_body_coords[0][1] + 2.0f;
 	}
 	else if (direction == DOWN) {
-		// Did we slither into ourself?
-		for (unsigned int a = 0; a < part_coords.size(); a++) {
-			if (part_coords[0][0] == part_coords[a][0] &&
-				part_coords[0][1] - 2.0f == part_coords[a][1]) {
+		// Snake hits itself?
+		for (unsigned int a = 0; a < snake_body_coords.size(); a++) {
+			if (snake_body_coords[0][0] == snake_body_coords[a][0] &&
+				snake_body_coords[0][1] - 2.0f == snake_body_coords[a][1]) {
 				exit(0);
 			}
 		}
 
-		// Did we slither into a wall?
-		if (part_coords[0][1] == -map_half_length) {
+		// Snake hits the wall?
+		if (snake_body_coords[0][1] == -map_half_length) {
 			exit(0);
 		}
 
-		// Did we get food?
-		if (part_coords[0][0] == food_coords[0] &&
-			part_coords[0][1] - 2.0f == food_coords[1]) {
+		// Snake gets the food?
+		if (snake_body_coords[0][0] == food_coords[0] &&
+			snake_body_coords[0][1] - 2.0f == food_coords[1]) {
 			growth_stage++;
+			move_speed-=10;
 			food_available = false;
 		}
 
-		new_head[1] = part_coords[0][1] - 2.0f;
+		new_head[1] = snake_body_coords[0][1] - 2.0f;
 	}
 	else {
-		new_head[1] = part_coords[0][1];
+		new_head[1] = snake_body_coords[0][1];
 	}
 
 	if (direction == LEFT) {
 		// Did we slither into ourself?
-		for (unsigned int a = 0; a < part_coords.size(); a++) {
-			if (part_coords[0][0] - 2.0f == part_coords[a][0] &&
-				part_coords[0][1] == part_coords[a][1]) {
+		for (unsigned int a = 0; a < snake_body_coords.size(); a++) {
+			if (snake_body_coords[0][0] - 2.0f == snake_body_coords[a][0] &&
+				snake_body_coords[0][1] == snake_body_coords[a][1]) {
 				exit(0);
 			}
 		}
 
 		// Did we slither into a wall?
-		if (part_coords[0][0] == -map_half_length) {
+		if (snake_body_coords[0][0] == -map_half_length) {
 			exit(0);
 		}
 
 		// Did we get food?
-		if (part_coords[0][0] - 2.0f == food_coords[0] &&
-			part_coords[0][1] == food_coords[1]) {
+		if (snake_body_coords[0][0] - 2.0f == food_coords[0] &&
+			snake_body_coords[0][1] == food_coords[1]) {
 			growth_stage++;
+			move_speed-=10;
 			food_available = false;
 		}
 
-		new_head[0] = part_coords[0][0] - 2.0f;
+		new_head[0] = snake_body_coords[0][0] - 2.0f;
 	}
 	else if (direction == RIGHT) {
 		// Did we slither into ourself?
-		for (unsigned int a = 0; a < part_coords.size(); a++) {
-			if (part_coords[0][0] + 2.0f == part_coords[a][0] &&
-				part_coords[0][1] == part_coords[a][1]) {
+		for (unsigned int a = 0; a < snake_body_coords.size(); a++) {
+			if (snake_body_coords[0][0] + 2.0f == snake_body_coords[a][0] &&
+				snake_body_coords[0][1] == snake_body_coords[a][1]) {
 				exit(0);
 			}
 		}
 
 		// Did we slither into a wall?
-		if (part_coords[0][0] == map_half_length) {
+		if (snake_body_coords[0][0] == map_half_length) {
 			exit(0);
 		}
 
 		// Did we get food?
-		if (part_coords[0][0] + 2.0f == food_coords[0] &&
-			part_coords[0][1] == food_coords[1]) {
+		if (snake_body_coords[0][0] + 2.0f == food_coords[0] &&
+			snake_body_coords[0][1] == food_coords[1]) {
 			growth_stage++;
+			move_speed-=10;
 			food_available = false;
 		}
 
-		new_head[0] = part_coords[0][0] + 2.0f;
+		new_head[0] = snake_body_coords[0][0] + 2.0f;
 	}
 	else {
-		new_head[0] = part_coords[0][0];
+		new_head[0] = snake_body_coords[0][0];
 	}
 
-	part_coords.push_front(new_head);
+	snake_body_coords.push_front(new_head);
 
 	if (!growth_stage) {
-		part_coords.pop_back();
+		snake_body_coords.pop_back();
 	}
 	else if (growth_stage == growth) {
 		growth_stage = 0;
@@ -316,14 +320,14 @@ int main(int argc, char** argv) {
 
 	int initSize = 3;
 
-	// Specify the coordinates to each part of the snake
+	// Drawing the snake
 	for (int a = 1; a <= initSize; a++) {
 		std::deque<float> row;
 
 		row.push_back(0.0f);
 		row.push_back((map_half_length + 2.0f + (initSize * 2)) - (a * 2));
 
-		part_coords.push_front(row);
+		snake_body_coords.push_front(row);
 	}
 
 	srand(time(NULL));
